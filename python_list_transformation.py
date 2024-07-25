ad7b41ad52fe1ccb7e8c41edeefb9d0f4be64390
adriangershfeld <adriangershfeld@protonmail.com>
2024-07-25 17:12:21 -0500

    first commit

---
+++ python_list_transformation.py
@@ -0,0 +1,12 @@
+# Task 1: Given the list of grades:
+# grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+# Sort the grades in descending order and print the sorted list.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
+
+# Task 2: Calculate the average grade and print it.
+
+average_grade = sum(grades) // len(grades)
+print(average_grade)
